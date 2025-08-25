46e6598b272589c62f53dc5ccbae4290aa7421fe
Vaibhavi Doshi <vaibhavidoshi2020@gmail.com>
2025-08-24 23:31:25 -0400

    Stop tracking node_modules and ignore it

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,9 @@
+exports.render = function (req, res) {
+  if (req.session.lastVisit) {
+    console.log('Last visit:', req.session.lastVisit);
+  }
+
+  req.session.lastVisit = new Date();
+
+  res.render('index', { title: 'Hello, Vaibhavi Doshi!' });
+};
---
+++ config/express.js
@@ -0,0 +1,43 @@
+const config = require('./config');
+const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const path = require('path');
+
+module.exports = function () {
+  const app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+  app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: config.sessionSecret
+  }));
+
+  // Views
+  app.set('views', path.join(__dirname, '../app/views'));
+  app.set('view engine', 'ejs');
+
+  // Serve your Windows folder at /img  <-- THIS LINE POINTS TO C:\Screenshot
+  app.use('/img', express.static(path.resolve('C:/Screenshot')));
+
+  // Also serve the project's /public if you use it for other assets
+  app.use(express.static(path.join(__dirname, '../public')));
+
+  // Routes
+  require('../app/routes/index.server.routes')(app);
+
+  return app;
+};
